f865963b63242de5928f30b2edb81f89649fe4f4
Henrique Silva <67487601+Henrique-William@users.noreply.github.com>
2025-05-19 23:19:14 +0000

    Bottom_Top - Otimizando Fibonacci com modelo Bottom Top

---
+++ Atividade/fibonnacci/Fibonacci_bottom_top.java
@@ -0,0 +1,31 @@
+import java.sql.Time;
+
+public class Fibonacci_bottom_top {
+    public static int fib(int n) {
+        if(n <= 1) return n;
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            System.out.println("Laço de calculo... calculando " + i);
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println("Tabulação "+ dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Digite o número que deseja calcular o Fibonacci: ");
+        int n = new java.util.Scanner(System.in).nextInt();
+
+        Time inicio = new Time(System.currentTimeMillis());
+        System.out.println("Fibonacci de " + n + ": " + fib(n));
+        Time fim = new Time(System.currentTimeMillis());
+
+        System.out.println("Inicio: " + inicio);
+        System.out.println("Fim: " + fim);
+        System.out.println("Tempo de execução: " + (fim.getTime() - inicio.getTime()) + "ms");
+    }
+}
